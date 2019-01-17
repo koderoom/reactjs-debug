f85dfb4b258ee6fa4d534bec02e5e073d2d83ac9
santosh <santosh.ece06@gmail.com>
2019-01-17 17:12:05 +0530

    udpate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {'todoList' : [1, 2, 3]};
+  }
+
   render() {
+    const itemList = this.state.todoList.map((item, index)=>
+        <li>1</li>
+    );
+
     return (
       <React.Fragment>
         <nav className="navbar navbar-dark bg-dark">
@@ -47,5 +56,8 @@ class App extends Component {
     );
   }
 }
+
+
+
 
 export default App;
