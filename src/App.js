ba4b7ce1877e9c6a53590aafed6a3130f0803776
santosh <santosh.ece06@gmail.com>
2019-01-17 11:26:25 +0530

    udpate.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">ReactJS Debug</a>
-      </nav>
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" style={{fontWeight:'bold'}} href="#">ReactJS Debug</a>
+        </nav>
+      </React.Fragment>
     );
   }
 }
