b9386ffaf237cc98a8f8e1166e18b3b6b16a52ee
santosh <santosh.ece06@gmail.com>
2019-01-17 16:42:41 +0530

    body

--- src/App.js
+++ src/App.js
@@ -10,13 +10,36 @@ class App extends Component {
         </nav>
 
         <div className="container-fluid">
-            <div className="row justify-content-center mt-1" >
-                <div className="col-6 bg-secondary rounded d-flex justify-content-center align-items-center" style={{height:'100px', opacity:'.9'}}>
-                    <input class="form-control form-control-lg col-10 mr-1" type="text" placeholder="Add New Todo" />
-                    <button type="button" class="btn btn-secondary " style={{fontWeight:'bold'}}>Add Todo</button>
+            <div className="row justify-content-center mt-2">
+                <div className="col-6 rounded">
+                    <div class="card">
+                      <div class="card-body bg-secondary d-flex justify-content-center align-items-center">
+                          <input class="form-control form-control-lg col-9 mr-1" type="text" placeholder="Add New Todo" />
+                          <button type="button" class="btn btn-lg btn-secondary ">Add Todo</button>
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div className="container-fluid">
+            <div className="row justify-content-center mt-4">
+              <div className="col-6">
+                <div class="card">
+                  <div class="card-header">
+                    Todo List
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
       </React.Fragment>
     );
   }
