55958ddc90f3450131c1f468d1a18bee358adcf9
santosh <santosh.ece06@gmail.com>
2019-01-17 16:32:49 +0530

    input box.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ class App extends Component {
         <nav className="navbar navbar-dark bg-dark">
           <a className="navbar-brand" style={{fontWeight:'bold'}} href="#">ReactJS Debug</a>
         </nav>
+
+        <div className="container-fluid">
+            <div className="row justify-content-center mt-1" >
+                <div className="col-6 bg-secondary rounded d-flex justify-content-center align-items-center" style={{height:'100px', opacity:'.9'}}>
+                    <input class="form-control form-control-lg col-10 mr-1" type="text" placeholder="Add New Todo" />
+                    <button type="button" class="btn btn-secondary " style={{fontWeight:'bold'}}>Add Todo</button>
+                </div>
+            </div>
+        </div>
       </React.Fragment>
     );
   }
